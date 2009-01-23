4655f4ef26f2c7ec4229d6ba95cc9fa187dfc918
bspaans <onderstekop@gmail.com>
2009-01-23 19:06:33 +0100

    Release often, release like a maniac.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name="improviser",
-	version="0.8.2.4",
+	version="0.8.3.0",
 	description="Automatic music generation software",
 	long_description="Experiments in musical content generation.",
 	author="Bart Spaans",
