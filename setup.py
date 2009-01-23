4663b9e92bfb307bc634ca8dc62c613fd772827b
bspaans <onderstekop@gmail.com>
2009-01-23 06:29:32 +0100

    Release 0.8.2.1; Updated setup script and CHANGELOG

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name="improviser",
-	version="0.8.2.0",
+	version="0.8.2.1",
 	description="Automatic music generation software",
 	long_description="Experiments in musical content generation.",
 	author="Bart Spaans",
